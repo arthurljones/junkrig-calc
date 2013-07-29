dfde560ab2d59c1895ed50e9e92aa72e266e89ac
Arthur L. Jones <arthur.l.jones@gmail.com>
2013-07-29 00:31:02 -0700

    Work on moment calculation

--- calculate_moment.py
+++ calculate_moment.py
@@ -1,4 +1,6 @@
-import math, numpy, time
+from math import *
+import time
+import numpy as np
 from PIL import Image
 
 def MakeImage(side, pixels_per_unit):
@@ -21,7 +23,6 @@ def PlotPixels(image, pixels, x, y, pixels_per_unit):
 
 def DiagonalMoment(outside, thickness, pixels_per_unit):
 	step_size = 1.0/pixels_per_unit
-	step_weight = 1.0/pixels_per_unit**2
 	cos_45 = 0.70710678118
 	r = thickness * 2
 	d_i = (outside - 2*r) * cos_45
@@ -32,16 +33,14 @@ def DiagonalMoment(outside, thickness, pixels_per_unit):
 	x_max = d_i + r
 
 	moment = 0.0
-
-	image, pixels = MakeImage(x_max, pixels_per_unit)
 	
-	for x in numpy.arange(x_min, x_max, step_size):
+	for x in np.arange(x_min, x_max, step_size):
 		y_min = 0.0
 		y_max = d_o - x
 		if x < y_max:
 			y_max = x
 
-		for y in numpy.arange(y_min, y_max, step_size):
+		for y in np.arange(y_min, y_max, step_size):
 			point_inside = False
 			if y <= x - d_i:
 				dist = (x - d_i)**2 + y**2
@@ -51,53 +50,75 @@ def DiagonalMoment(outside, thickness, pixels_per_unit):
 					point_inside = True
 			
 			if point_inside:
-				moment = moment + (y**2 + x**2) * step_weight
-				PlotPixels(image, pixels, x, y, pixels_per_unit)
-	
-	image.save("moment_diagonal.png", dpi=(pixels_per_unit, pixels_per_unit))
-	return moment * 4
+				moment += (y*y + x*x)
 
+	return (moment / (pixels_per_unit * pixels_per_unit)) * 4
 
-def StraightMoment(outside, thickness, pixels_per_unit):
+def Moment(outside, thickness, angle, pixels_per_unit):
+	sin_t = sin(radians(angle))
+	cos_t = cos(radians(angle))
+
+	cos_45 = 0.70710678118
 	step_size = 1.0/pixels_per_unit
-	step_weight = 1.0/pixels_per_unit**2
 	r = thickness * 2
 	d_o = outside / 2
 	d_m = d_o - thickness
 	d_i = d_o - r
 
-	x_min = d_i
-	x_max = d_o
+	x_min = 0
+	x_max = outside * cos_45
 
-	moment = 0.0
+	y_min = 0
+	y_max = outside * cos_45
 
-	image, pixels = MakeImage(d_o, pixels_per_unit)
+	r_squared = r**2
 	
-	for x in numpy.arange(x_min, x_max, step_size):
-		y_min = 0
-		if x < d_m:
-			y_min = d_i
-		y_max = x
-		
-		for y in numpy.arange(y_min, y_max, step_size):
+	moment = 0
+	points = []
+
+	for x in np.arange(x_min, x_max, step_size):
+		for y in np.arange(y_min, y_max, step_size):
+
+			tx = x*cos_t - y*sin_t
+			ty = x*sin_t + y*cos_t
+
+			if tx < 0 and ty < 0:
+				tx, ty = -tx, -ty
+				ty = -ty
+			elif tx < 0:
+				tx, ty = ty, -tx
+			elif ty < 0:
+				tx, ty = -ty, tx
+
 			point_inside = False
-			if y > d_i:
-				dist = (x - d_i)**2 + (y - d_i)**2
-				if dist <= r**2:
-					point_inside = True
+			if tx < d_i:
+				point_inside = (d_m <= ty <= d_o)
+			elif ty < d_i:
+				point_inside = (d_m <= tx <= d_o)
 			else:
-				point_inside = True
+				dx = tx - d_i
+				dy = ty - d_i
+				point_inside = dx*dx + dy*dy <= r_squared
 
-			#pixels[int(x/step_size), int(y/step_size)] = 0x000000AA
 			if point_inside:
-				moment = moment + (y**2 + x**2) * step_weight
-				PlotPixels(image, pixels, x, y, pixels_per_unit)
+				moment += y * y
 
-	image.save("moment_straight.png", dpi=(pixels_per_unit, pixels_per_unit))
+	moment /= pixels_per_unit * pixels_per_unit
+
 	return moment * 4
 
-args = (10, 1.5, 100)
+def StraightMoment(outside, thickness, pixels_per_unit):
+	return Moment(outside, thickness, 0, pixels_per_unit)
+
+outside = 10
+thickness = 1.5
+ppu = 50
+
 #print "diagonal moment: {}".format(DiagonalMoment(*args))
 start = time.clock()
-print "straight moment: {}".format(StraightMoment(*args))
+moment = Moment(outside, thickness, 45, ppu)
+dmoment = moment - DiagonalMoment(outside, thickness, ppu)
+print dmoment
+difference = moment - dmoment#560.64903168
+print "straight moment: {} ({})".format(moment, difference)
 print "time: {}".format(time.clock() - start)
