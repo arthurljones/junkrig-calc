6b81d4efc85ccca923627fb479a4bd0f53da3d6a
Arthur Jones <arthur.l.jones@gmail.com>
2014-11-27 20:26:56 -0800

    More unit tests

--- spec/src/cross_sections/tube_spec.rb
+++ spec/src/cross_sections/tube_spec.rb
@@ -9,12 +9,57 @@ RSpec.describe CrossSections::Tube do
     }
   end
 
+  def default_tube
+    CrossSections::Tube.new(default_options)
+  end
+
   describe "#initialize" do
-    it "constructs a valid object" do
-      tube = CrossSections::Tube.new(default_options)
+    it "constructs an object with the passed parameters" do
+      tube = default_tube
 
       expect(tube.outer_diameter).to eq Unit(default_options[:outer_diameter])
       expect(tube.wall_thickness).to eq Unit(default_options[:wall_thickness])
     end
+
+    it "converts compatible units" do
+      options = default_options
+      options[:outer_diameter] = "25.4 mm"
+      tube = CrossSections::Tube.new(options)
+      expect(tube.outer_diameter).to eq "1 in"
+    end
+
+    it "disallows mismatched units" do
+      options = default_options
+      options[:outer_diameter] = "10 seconds"
+      expect{CrossSections::Tube.new(options)}.to raise_error
+    end
+
+    it "disallows invalid values" do
+      pending "implementation of this validation"
+      options = default_options
+      options[:outer_diameter] = "-5 in"
+      expect{CrossSections::Tube.new(options)}.to raise_error
+    end
+
   end
+
+  describe "#inner_diameter" do
+    it "returns the inner diameter" do
+      expect(default_tube.inner_diameter).to eq Unit("0.75 in")
+    end
+  end
+
+  describe "#outer_radius" do
+    it "returns the outer radius" do
+      expect(default_tube.outer_radius).to eq Unit("0.5 in")
+    end
+  end
+
+  describe "#inner_radius" do
+    it "returns the inner radius" do
+      expect(default_tube.inner_radius).to eq Unit("0.375 in")
+    end
+  end
+
+
 end
