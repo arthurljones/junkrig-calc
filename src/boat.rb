7eb5dbcf29a3fd97578abf7996524301374e6409
Arthur Jones <arthur.l.jones@gmail.com>
2014-12-02 18:53:00 -0800

    Refactor boat

--- src/boat.rb
+++ src/boat.rb
@@ -3,6 +3,17 @@ require "options_initializer"
 class Boat
   include OptionsInitializer
 
+  attr_reader *%i(
+    saltwater_displaced
+    ballast_ratio
+    screening_stability_value
+    stability_range
+    displacement_to_length
+    buoyancy_lever
+    estimated_max_righting_moment
+    water_pressure_at_keel
+  )
+
   options_initialize(
     displacement: { units: "lbs" },
     ballast: { units: "lbs" },
@@ -15,52 +26,26 @@ class Boat
     buoyancy_lever: { :required => false, units: "in" },
     foredeck_angle: { units: "deg"},
   ) do |options|
-
+    #volume of seawater displaced by the boat"
+    @saltwater_displaced = (displacement / Constants.saltwater_density).to("m^3")
+    #ratio of ballast to total displacement"
+    @ballast_ratio = ballast / displacement
+    #composite value that represents the stability of the boat"
+    @screening_stability_value = maximum_beam.to("in")**2 / (ballast_ratio * draft_of_canoe_body * saltwater_displaced**(1/3)).to("in^2")
+    #angle at which the boat loses upright stability"
+    @stability_range = Unit(110 + (400 / (screening_stability_value - 10)), "degrees")
+    #composite ratio relating the displacement to the waterline length"
+    @displacement_to_length = displacement.to("long-tons").scalar / (length_at_waterline.to("ft").scalar / 100)**3
+    #buoyancy lever specified on construction, if it exists"
+    @buoyancy_lever = @buoyancy_lever || maximum_beam / 4
+    #righting moment if the boat was knocked down, but the center of buoyancy stayed the same"
+    #TODO: This assumes that the center of buoyancy doesn't move as the boat heels, which is technically wrong.
+    @estimated_max_righting_moment = buoyancy_lever * displacement
+    #saltwater pressure outside the hull at the top of the keel"
+    @water_pressure_at_keel = (Constants.saltwater_density * Constants.gravity * draft_of_canoe_body).to("psi")
   end
 
   def self.from_file(file)
     new(YAML.load_file(file).with_indifferent_access)
   end
-
-  "returns the volume of seawater displaced by the boat"
-  def saltwater_displaced
-    (displacement / Constants.saltwater_density).to("m^3")
-  end
-
-  "returns the ratio of ballast to total displacement"
-  def ballast_ratio
-    ballast / displacement
-  end
-
-  "returns a composite value that represents the stability of the boat"
-  def screening_stability_value
-    maximum_beam.to("in")**2 / (ballast_ratio * draft_of_canoe_body * saltwater_displaced**(1/3)).to("in^2")
-  end
-
-  "returns the angle at which the boat loses upright stability"
-  def stability_range
-    Unit(110 + (400 / (screening_stability_value - 10)), "degrees")
-  end
-
-  "returns a composite ratio relating the displacement to the waterline length"
-  def displacement_to_length
-    displacement.to("long-tons").scalar / (length_at_waterline.to("ft").scalar / 100)**3
-  end
-
-  "returns the buoyancy lever specified on construction, if it exists"
-  def buoyancy_lever
-    @buoyancy_lever || maximum_beam / 4
-  end
-
-  "returns the righting moment if the boat was knocked down, but the center of buoyancy stayed the same"
-  def estimated_max_righting_moment
-    #TODO: This assumes that the center of buoyancy doesn't move as the boat heels, which is technically wrong.
-    buoyancy_lever * displacement
-  end
-
-  "returns the saltwater pressure outside the hull at the top of the keel"
-  def water_pressure_at_keel
-    (Constants.saltwater_density * Constants.gravity * draft_of_canoe_body).to("psi")
-  end
-
 end
