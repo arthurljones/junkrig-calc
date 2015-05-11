486b50e43b01966ae1e2d95708de9ee63becafe4
Arthur Jones <arthur.l.jones@gmail.com>
2015-05-10 23:05:39 -0700

    Improve sail export

--- export/sail.rb
+++ export/sail.rb
@@ -4,11 +4,28 @@ require "export_helper"
 
 sail = Sail::Sail.new(load_yaml_data_file("sail.yml"))
 output_format = [
-    [:area, "ft^2"],
-    [:panel_width, "ft"],
+    [:parallelogram_luff, "ft"],
+    [:batten_length, "ft"],
+    [:lower_panel_count, "nil"],
+    [:head_panel_count, "nil"],
+    [:yard_angle, "deg"],
+    [nil],
+    [:lower_panel_luff, "ft"],
+    [:head_panel_luff, "in"],
+    [:total_luff, "ft"],
     [:total_leech, "ft"],
-    [:center_above_tack, "ft"],
-    [:center_before_tack, "ft"],
+    [:parallelogram_width, "ft"],
+    [:circumference, "ft"],
+    [:luff_to_batten_length, nil],
+    [nil],
+    [:tack_angle, "deg"],
+    [:clew_rise, "ft"],
+    [:center_above_tack, "ft", ->(obj){ obj.center.y }],
+    [:center_before_tack, "ft", ->(obj){ obj.center.x }],
+    [:yard_vertical_span, "ft", ->(obj){ obj.yard_span.y }],
+    [:yard_horizontal_span, "ft", ->(obj){ obj.yard_span.x }],
+    [:peak_above_tack, "ft", ->(obj){ obj.tack_to_peak.y }],
+    [:aspect_ratio, nil],
 ]
 
 puts ExportHelper.generate_csv(sail, output_format)
@@ -25,6 +42,7 @@ objects = (0..sail.total_panels).collect{|p| ReefedPanelArea.new(p, sail.reefed_
 output_format = [
     [:reefed_panels, nil],
     [:sail_area, "ft^2"],
+    [:sail_area, "m^2"],
 ]
 
 puts
