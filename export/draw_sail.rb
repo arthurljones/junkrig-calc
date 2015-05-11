22c433d8319b1a225e81f408a8335e3beefc8d4d
Arthur Jones <arthur.l.jones@gmail.com>
2015-05-10 18:49:45 -0700

    Move files into export directory

---
+++ export/draw_sail.rb
@@ -0,0 +1,7 @@
+require_relative "../boilerplate"
+require_relative "../sail/sail"
+
+root = File.expand_path(File.dirname(__FILE__))
+sail_data = YAML.load_file(File.join(root, "../sail.yml")).with_indifferent_access
+sail = Sail::Sail.new(sail_data)
+sail.draw_to_file(ARGV[0] || "sail.svg")
