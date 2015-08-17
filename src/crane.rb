2006ad0b4b8dedc00064822df56acc4adeb8aada
Arthur Jones <arthur.l.jones@gmail.com>
2015-08-16 19:28:59 -0700

    Rename a variable and simplify calculations

--- src/crane.rb
+++ src/crane.rb
@@ -15,14 +15,13 @@ class Crane
 
     #TODO: Pull these from the initializer
     @running_line_purchase = 6
-    @pulley_to_mast_pivot_along_deck = Unit.new("128 in")
+    @mast_pivot_to_pulley = Vector2.new(Unit.new("110 in"), Unit.new("-8 in"))
+    @mast_pivot_to_crane_pivot = Vector2.new(Unit.new("-52 in"), Unit.new("-19 in"))
     @crane_height = Unit.new("14 ft")
-    @pivot_moves_with_mast = false
     @crane_base_width = Unit.new("70 in")
     @crane_crossmember_height = Unit.new("86 in")
-    @initial_crane_angle = Unit.new("90 deg")
-    @mast_pivot_height_above_crane_pivot = Unit.new("18 in")
-    @guy_pivot_above_pivot = Unit.new("31.7 ft")
+    @initial_crane_angle = Unit.new("80 deg")
+    @guy_anchor_above_pivot = Unit.new("31.7 ft")
     @mast_center_to_pivot_x = Unit.new("-0.75 ft")
     @mast_foot_to_pivot_y = Unit.new("3.3 ft")
     @mast_center_of_mass_above_foot = Unit.new("183.34 in")
@@ -33,7 +32,7 @@ class Crane
     #TODO: Smoother curve instead of sharp cutout (which would physically look like a spiral section)
     max_chain_basket_lift = Unit.new("3 ft")
     chain_weight = Unit.new("300 lbs")
-    @lift_assist_torque = @mast_foot_to_pivot_y * chain_weight
+    @lift_assist_torque = Unit.new("0 ft*lbs") #@mast_foot_to_pivot_y * chain_weight
     @lift_assist_torque_cutout_angle = Unit.new(@mast_foot_to_pivot_y / max_chain_basket_lift, "rad")
 
     #Mast Pivot
@@ -45,20 +44,13 @@ class Crane
 
     #NOTE: This coordinate system has the mast pivot at (0, 0)
 
-    #Pulley
-    @foredeck_angle = Unit.new("3 deg")
-    along_deck = Vector2.new(@pulley_to_mast_pivot_along_deck, zero_dist)
-    pivot_to_deck = Vector2.new(zero_dist, -@pivot_height_above_deck)
-    @mast_pivot_to_pulley = along_deck.rotated_by(@foredeck_angle) + pivot_to_deck
-
     #Crane
-    @crane_pivot_zero = Vector2.new(-(@crane_height - @pulley_to_mast_pivot_along_deck), -(@mast_pivot_height_above_crane_pivot))
-    @crane_tip_initial = @crane_pivot_zero + Vector2.new(@crane_height, zero_dist).rotated_by(@initial_crane_angle)
+    @crane_tip_initial = @mast_pivot_to_crane_pivot + Vector2.new(@crane_height, zero_dist).rotated_by(@initial_crane_angle)
 
     #Guy
-    @guy_pivot_zero = Vector2.new(@guy_pivot_above_pivot, zero_dist)
-    @guy_pivot_initial = -@guy_pivot_zero #TODO: Simplify this so only the zero or inital is used (at least one is misnamed)
-    @guy_length = (@guy_pivot_initial - @crane_tip_initial).magnitude #D7
+    @guy_anchor_zero = Vector2.new(@guy_anchor_above_pivot, zero_dist)
+    @guy_anchor_initial = -@guy_anchor_zero #TODO: Simplify this so only the zero or inital is used (at least one is misnamed)
+    @guy_length = (@guy_anchor_initial - @crane_tip_initial).magnitude #D7
 
   end
 
@@ -71,43 +63,38 @@ class Crane
     static_torque = (-@pivot_to_mast_cm_initial.rotated_by(angle) * @mast_weight).x
     static_torque += @lift_assist_torque if angle_above_horizontal <= @lift_assist_torque_cutout_angle
 
-    crane_pivot = @crane_pivot_zero
-    crane_pivot = crane_pivot.rotated_by(angle_above_horizontal) if @pivot_moves_with_mast #TODO: Why is it using this angle that's 180º out?
-    guy_pivot = @guy_pivot_zero.rotated_by(angle)
-    guy_pivot_to_crane_pivot = crane_pivot - guy_pivot #C11
-    guy_pivot_to_crane_pivot_length = guy_pivot_to_crane_pivot.magnitude
-    γ = Math::acos((guy_pivot_to_crane_pivot_length ** 2 + @crane_height ** 2 - @guy_length ** 2) / (-2 * @crane_height * guy_pivot_to_crane_pivot_length))
+    crane_pivot = @mast_pivot_to_crane_pivot
+    guy_anchor = @guy_anchor_zero.rotated_by(angle)
+    guy_anchor_to_crane_pivot = crane_pivot - guy_anchor
+    guy_anchor_to_crane_pivot_length = guy_anchor_to_crane_pivot.magnitude
+    γ = Math::acos((guy_anchor_to_crane_pivot_length ** 2 + @crane_height ** 2 - @guy_length ** 2) / (-2 * @crane_height * guy_anchor_to_crane_pivot_length))
 
-    crane_angle = guy_pivot_to_crane_pivot.angle + Unit.new(γ, "rad")
-    crane_vector = Vector2.new(@crane_height, "0 in").rotated_by(crane_angle) #C14
+    crane_angle = guy_anchor_to_crane_pivot.angle + Unit.new(γ, "rad")
+    crane_vector = Vector2.new(@crane_height, "0 in").rotated_by(crane_angle)
 
-    crane_tip_to_guy_pivot_vector = guy_pivot_to_crane_pivot + crane_vector
-    guy_angle = Unit.new((crane_tip_to_guy_pivot_vector.to("ft").unitless.to_complex / Complex(@guy_length.to("ft").scalar, 0)).angle, "rad") #C17
-    guy_angle_to_crane = crane_angle - guy_angle #C18
-    guy_torque_lever = guy_pivot_to_crane_pivot_length * Math::sin(guy_pivot_to_crane_pivot.angle - guy_angle) #C19
+    crane_tip_to_guy_anchor_vector = guy_anchor_to_crane_pivot + crane_vector
+    guy_angle = Unit.new((crane_tip_to_guy_anchor_vector.to("ft").unitless.to_complex / Complex(@guy_length.to("ft").scalar, 0)).angle, "rad")
+    guy_angle_to_crane = crane_angle - guy_angle
+    guy_torque_lever = guy_anchor_to_crane_pivot_length * Math::sin(guy_anchor_to_crane_pivot.angle - guy_angle)
 
-    crane_torque_lever = Unit.new("0 in") #C15
-    if @pivot_moves_with_mast
-      crane_torque_lever = crane_pivot.magnitude * Math::sin(guy_pivot.angle) - guy_angle
-    end
-
+    crane_torque_lever = Unit.new("0 in")
     lever_vector = crane_pivot + crane_vector
 
     line_vector = lever_vector - @mast_pivot_to_pulley
     crane_tip_above_pulley = line_vector.dot(Vector2.new("0 in", "1 in")) > 0
     line_length = line_vector.magnitude
     line_angle = line_vector.angle
-    line_angle_to_crane = line_angle - crane_angle #C29 #TODO: This seems backward or at least misnamed
-    line_angle_to_guy = line_angle_to_crane + guy_angle_to_crane #C30
+    line_angle_to_crane = line_angle - crane_angle #TODO: This seems backward or at least misnamed
+    line_angle_to_guy = line_angle_to_crane + guy_angle_to_crane
 
     line_force = static_torque * Math::sin(guy_angle_to_crane) /
-      (Math::sin(line_angle_to_crane) * guy_torque_lever + crane_torque_lever * Math::sin(line_angle_to_guy)) #C32
+      (Math::sin(line_angle_to_crane) * guy_torque_lever + crane_torque_lever * Math::sin(line_angle_to_guy))
 
-    crane_force = line_force * Math::sin(line_angle_to_guy) / Math::sin(guy_angle_to_crane) #C33
+    crane_force = line_force * Math::sin(line_angle_to_guy) / Math::sin(guy_angle_to_crane)
 
     guy_force = (static_torque - crane_force * crane_torque_lever) / guy_torque_lever
 
-    guy_pivot_position = guy_pivot
+    guy_anchor_position = guy_anchor
     crane_tip_position = lever_vector
     crane_pivot_position = crane_pivot
     pulley_position = @mast_pivot_to_pulley
@@ -120,7 +107,7 @@ class Crane
       line_force: line_force,
       crane_force: crane_force,
       guy_force: guy_force,
-      guy_pivot: guy_pivot_position,
+      guy_anchor: guy_anchor_position,
       crane_tip: crane_tip_position,
       crane_pivot: crane_pivot_position,
       pulley: pulley_position,
