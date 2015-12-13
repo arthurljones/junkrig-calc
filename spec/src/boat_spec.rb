bdca934e38491c2aee92968caee30ecb4af3d2d3
Arthur Jones <arthur.l.jones@gmail.com>
2015-12-12 19:49:40 -0800

    Fix some units in spec

--- spec/src/boat_spec.rb
+++ spec/src/boat_spec.rb
@@ -32,13 +32,13 @@ RSpec.describe Boat do
     its(:stability_range) { is_expected.to be_within("0.01 deg").of "142.11 deg" }
     its(:displacement_to_length) { is_expected.to be_within(0.01).of 203.37 }
     its(:max_buoyancy_lever) { is_expected.to be_within(delta "in").of "2.75 ft" }
-    its(:estimated_max_righting_moment) { is_expected.to be_within(delta "in*lbs").of "3.3e5 in*lbs" }
+    its(:estimated_max_righting_moment) { is_expected.to be_within(delta "in*lbf").of "3.3e5 in*lbf" }
     its(:water_pressure_at_keel) { is_expected.to be_within("0.01 psi").of "0.89 psi" }
 
     context "with a specified max_buoyancy_lever" do
       subject! { default_boat(:max_buoyancy_lever => "18 in") }
       its(:max_buoyancy_lever) { is_expected.to eq "18 in" }
-      its(:estimated_max_righting_moment) { is_expected.to be_within(delta "in*lbs").of "1.8e5 in*lbs" }
+      its(:estimated_max_righting_moment) { is_expected.to be_within(delta "in*lbf").of "1.8e5 in*lbf" }
     end
   end
 end
